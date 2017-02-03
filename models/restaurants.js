32ba675b0f6f86b02ace224de79abf324945383b
Elijah <deasisjohnelijah@gmail.com>
2017-02-03 16:21:24 +0800

    module-5

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,28 +1,24 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const restify = require('express-restify-mongoose');
+const app = express();
+const router = express.Router();
 
-var RestaurantSchema = new Schema({
-  // id is created automatically
-  name: {
-    type: String,
-    required: [true, 'Fill up Name']
-    
-  },
-  address: {
-    type: String,
-    required: [true, 'Fill up Address']
-  },
- 
-  description: String,
-  cuisine: String,
-  maplink: String,
-  contact: String,
-  zomato: String,
-  photo: String,
-  photoslink: String,
-  createdate: String,
-  updatedate: String,
+app.use(bodyParser.json());
+app.use(methodOverride());
 
+mongoose.connect('mongodb://elijahdeasis:elijahdeasis30@ds111469.mlab.com:11469/coen3463-t12');
+
+restify.serve(router, mongoose.model('Customer', new mongoose.Schema({
+  name: { type: String, required: true },
+  comment: { type: String }
+})));
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000')
 });
-
-module.exports = mongoose.model('Restaurant', RestaurantSchema);
+module.exports = app;
