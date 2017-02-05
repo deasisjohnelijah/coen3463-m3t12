2d591a56c72e607c1336bce369d4a3eb72b0437d
Elijah <deasisjohnelijah@gmail.com>
2017-02-05 20:51:40 +0800

    edit

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Top Manila Restaurants 2017' });
+  res.render('about', { title: 'Top Manila Restaurants 2017',  user: req.user});
 });
 
 module.exports = router;
